37f2b1dbf442a1d40363ebd16146f2c447487c8f
Валерия Михайлова <valeriya23.04@outlook.com>
2022-11-28 13:52:43 +0300

    Добавьте файлы проекта.

---
+++ aip12aip/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp12
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int> { -12, 0, -4, 0, 0, 0, 5, -12, 0, -5, 0, 0, 4 }; //дана последовательность
+
+            int nonZeroCount = nums.Where(num => num != 0).Count(); //переменная целого типа
+
+            Console.WriteLine($"Количество ненулевых чисел равно {nonZeroCount}");  //программа считает 
+
+            Console.ReadKey(); //вывод на консоль
+        }
+    }
+}
